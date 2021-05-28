d2d72a34fc7355cfe16d0044cab81d6699b4d557
Thong Cam <thongcam02@gmail.com>
2021-05-28 14:40:42 +0700

    Add light turning functions

--- new-nuc.py
+++ new-nuc.py
@@ -1,10 +1,19 @@
-
+from typing_extensions import ParamSpecKwargs
 import socketio
 import subprocess
 
 
 IP_ADDRESS = "192.168.1.12"
 DEVICE_ID = "USB\VID_05AC&PID_024F&REV_0124"
+
+
+def turnLightOn():
+    pass
+
+
+def turnLightOff():
+    pass
+
 
 sio = socketio.Client()
 sio.connect(f"http://{IP_ADDRESS}:8080/nuc")
@@ -24,7 +33,6 @@ def disconnect():
 def on_control_signal(data):
     print(data["signal"])
     if data["signal"] == "ON":
-        returnObject = subprocess.run(["./devcon.exe", "rescan"])
+        turnLightOn()
     else:
-        returnObject = subprocess.run(
-            ["./devcon.exe", "remove", DEVICE_ID])
+        turnLightOff()
